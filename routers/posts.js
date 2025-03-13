f0bcacbd70ddb4043e79fc84f3a51f726d453342
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-13 13:23:53 +0100

    add route with CRUD

--- routers/posts.js
+++ routers/posts.js
@@ -1,2 +1,34 @@
 const express = require('express');
 const router = express.Router();
+
+// index
+router.get('/', (req, res) => {
+    res.send('Return all posts here')
+})
+
+// show
+router.get('/:post', (req, res) => {
+    res.send(`Show post: ${req.params.post}`)
+})
+
+// store
+router.post('/', (req, res) => {
+    res.send('Store a new post')
+})
+
+// update
+router.put('/:post', (req, res) => {
+    res.send(`Update post: ${req.params.post}`)
+})
+
+// modify
+router.patch('/:post', (req, res) => {
+    res.send(`Modify post: ${req.params.post}`)
+})
+
+// delete
+router.delete('/:post', (req, res) => {
+    res.send(`Delete post: ${req.params.post}`)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const postsRouter = require('./routers/posts')
+
 // listening
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
     
 })
 
-// homepage rout
+// homepage route
 app.get('/', (req, res) => {
     res.send('Welcome to our server')
 })
+
+// posts route
+app.use('/api/v1/posts', postsRouter)
